f7788c7e23ab4896d96dcfd987052491f1b351c9
Calvin B. McCarter <calvin199@gmail.com>
2015-05-01 03:32:44 -0400

    cleanup bcgd_ha

--- addKernelRegression/bcgd_ha.m
+++ addKernelRegression/bcgd_ha.m
@@ -31,7 +31,7 @@ function [optBeta, optStats] = bcdExact(Ls, Y, lambda, params)
     end
     
     for iter=1:params.maxNumIters
-        for g=1:m
+        for g=randperm(m)
             Beta_g = Beta(:,g);
             L_beta_g = L_beta(:,g);
             L_g =  Ls(:,:,g);
@@ -70,29 +70,4 @@ function [optBeta, optStats] = bcdExact(Ls, Y, lambda, params)
     optStats.objective = objHistory;
     optStats.time = timeHistory;
     optBeta = Beta;
-
-function ignore()
-            f_g = 1/(2*n)*norm(y_minus_other-L_beta_g,2)^2 + ...
-                (lambda/m)*norm(Beta_g,2);
-            f_g = objective(Beta);
-            delta = 1*grad_g'*d_g + norm(Beta_g + d_g,2) - norm(Beta_g, 2);
-            %assert(delta < 0);
-            step = 1;
-            success = 0;
-            for lsiter=1:params.max_ls_iters
-                Beta_step = Beta_g + step*d_g;
-                L_Beta_step = L_g'*Beta_step;
-                f_step = 0.5*norm(y_minus_other-L_Beta_step,2)^2 + ...
-                    (lambda/m)*norm(Beta_step,2);
-                Beta_step = Beta;
-                Beta_step(:,g) = Beta_g + step*d_g;
-                f_step = objective(Beta_step);
-                if f_step <= f_g + 0.001*step*delta
-                    success = 1;
-                    break;
-                end
-                step = 0.5*step;
-            end
-            %assert(success == 1);
-end
 end
