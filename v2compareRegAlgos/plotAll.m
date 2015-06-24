4b7c1740b824aaff81158ea8100cb9addaa7c05e
samy <kandasamy@cmu.edu>
2015-06-23 20:15:59 -0400

    changes

--- v2compareRegAlgos/plotAll.m
+++ v2compareRegAlgos/plotAll.m
@@ -1,7 +1,10 @@
 
 clear all;
 dirName = 'results';
-dirSearchName = sprintf('%s/v2-*.mat', dirName);
+% dirSearchName = sprintf('%s/v2-*.mat', dirName);
+% dirSearchName = sprintf('%s/v2new-0602-*.mat', dirName);
+% dirSearchName = sprintf('%s/v2new-0603-*.mat', dirName);
+dirSearchName = sprintf('%s/v2new-0604-*.mat', dirName);
 files = dir(dirSearchName);
 
 % Now add all of it
@@ -9,6 +12,7 @@ for i = 1:numel(files)
   fileName = sprintf('%s/%s', dirName, files(i).name);
   load(fileName);
 %   saveFileName = fi;
+  fileName,
   plotV2ResultsDuplicate;
   pause;
 
